7892c75bbf3003ed6688bdf91f67b4c0cb51c746
Satriaaahsn <Ravickys94@gmail.com>
2026-06-30 15:05:52 +0800

    Menambahkan tampilan beranda

---
+++ views/beranda.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+<meta charset="UTF-8">
+<title>Klinik Harmoni Medika</title>
+<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+<div class="container">
+    <a class="navbar-brand" href="#">Klinik Harmoni Medika</a>
+    <div class="collapse navbar-collapse">
+    <ul class="navbar-nav ms-auto">
+        <li class="nav-item"><a class="nav-link" href="index.php">Beranda</a></li>
+        <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
+        <li class="nav-item"><a class="nav-link" href="register.php">Daftar</a></li>
+    </ul>
+    </div>
+</div>
+</nav>
+
+<div class="container text-center mt-5">
+<h2>Halo, Selamat Datang</h2>
+<p>Kesehatan Anda Prioritas Kami</p>
+<p>Klinik Harmoni Medika siap melayani dengan ramah dan profesional.</p>
+<button class="btn btn-primary">Mulai Login</button>
+<button class="btn btn-outline-primary">Daftar Pasien</button>
+</div>
+
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
